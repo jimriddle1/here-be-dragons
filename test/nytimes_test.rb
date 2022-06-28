61dbdf24e4b77b7bbc7de185d68c59f49b7a1bb5
Jim Riddle <99755958+jimriddle1@users.noreply.github.com>
2022-06-28 09:44:42 -0500

    Update nytimes_test.rb
    got tests to pass

--- test/nytimes_test.rb
+++ test/nytimes_test.rb
@@ -1,6 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require 'pry'
+# require 'pry'
 require './lib/nytimes'
 
 class NytimesTest < Minitest::Test
@@ -12,13 +12,13 @@ class NytimesTest < Minitest::Test
 
   def test_it_can_get_copyright
     #Set your code to the local variable, "result"
-
+    result = @hash[:copyright]
     assert result, "Copyright (c) 2018 The New York Times Company. All Rights Reserved."
   end
 
   def test_it_can_get_array_of_stories
     #Set your code to the local variable, "result"
-
+    result = @hash[:results]
     assert result.is_a? (Array)
     assert_equal 44, result.count
   end
@@ -26,7 +26,10 @@ class NytimesTest < Minitest::Test
   def test_it_can_get_all_stories_with_subsection_of_politics
     #Set your code to the local variable, "result"
 
-
+    all_stories = @hash[:results]
+    result = all_stories.select do |story|
+      story[:subsection] == "Politics"
+    end
     assert result.is_a? (Array)
     assert_equal 6, result.count
     assert_equal "Congressional G.O.P. Agenda Quietly Falls Into Place Even as Trump Steals the Spotlight",
